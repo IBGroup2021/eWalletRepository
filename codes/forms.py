eb0566461df36742dfe4213c28250dfaed608e43
IBGroup2021 <79875697+IBGroup2021@users.noreply.github.com>
2021-03-08 11:10:15 -0500

    Delete codes directory

--- codes/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Codigo
-
-class CodeForm(forms.ModelForm):
-    class Meta:
-        number = forms.CharField(label = 'code', help_text='Entra codigo de verificacion')
-        model = Codigo
-        fields = ('number',)
